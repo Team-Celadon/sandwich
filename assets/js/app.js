4365c4aafbde247ecf8a3f1a6ba006ce110e35fa
Moon Child <kaitline.moon@gmail.com>
2018-11-17 10:47:57 -0500

    Updated orm, app.js, routes, started passport, new user added works.

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,10 +26,6 @@ $(document).ready(function () {
         }
     })
 
-})
-
-
-
 $(document).on("click", "#submitSandwich", function (event) {
     event.preventDefault();
     var toppingsArray = [];
@@ -85,8 +81,29 @@ $(document).on("click", "#submitSandwich", function (event) {
             
         })
     }
+})
 
 
 
     $("#sandwichInfo").show();
+
+// On click event for submitting a new user
+    $(document).on("click", "#submit-newuser", function (event) {
+        event.preventDefault()
+        console.log('this was registered')
+        var newName = $("#username").val().trim()
+        var newPhone = $("#phone").val()
+        var newPassword = $("#password").val().trim()
+
+        $.ajax("/login", {
+            type: "POST",
+            data: { username: newName, phone: newPhone, password: newPassword }
+        }).then(
+            function () {
+                console.log('Added a new user to the database!')
+
+                window.location = '/features'
+            }
+        )
+    })
 })
